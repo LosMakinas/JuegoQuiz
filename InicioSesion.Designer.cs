39aaa4c7c7b611a684dea4e370fcb80c7da1e534
AitorRamos17 <ramossantosaitor@gmail.com>
2023-05-10 09:40:33 +0200

    Inicio Sesion sin q lleve a otro formulario

---
+++ InicioSesion.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace JuegoQuizzReto
+{
+    partial class InicioSesion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIniarSesion = new System.Windows.Forms.Button();
+            this.txtNombreUsu = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(191, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PHOTO PLAY";
+            // 
+            // btnIniarSesion
+            // 
+            this.btnIniarSesion.BackColor = System.Drawing.Color.Transparent;
+            this.btnIniarSesion.FlatAppearance.BorderSize = 0;
+            this.btnIniarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniarSesion.Location = new System.Drawing.Point(269, 355);
+            this.btnIniarSesion.Name = "btnIniarSesion";
+            this.btnIniarSesion.Size = new System.Drawing.Size(156, 43);
+            this.btnIniarSesion.TabIndex = 1;
+            this.btnIniarSesion.Text = "Iniciar Sesión";
+            this.btnIniarSesion.UseVisualStyleBackColor = false;
+            this.btnIniarSesion.Click += new System.EventHandler(this.btnIniarSesion_Click);
+            this.btnIniarSesion.MouseEnter += new System.EventHandler(this.btnIniarSesion_MouseEnter);
+            this.btnIniarSesion.MouseLeave += new System.EventHandler(this.btnIniarSesion_MouseLeave);
+            // 
+            // txtNombreUsu
+            // 
+            this.txtNombreUsu.Location = new System.Drawing.Point(218, 281);
+            this.txtNombreUsu.Name = "txtNombreUsu";
+            this.txtNombreUsu.Size = new System.Drawing.Size(317, 20);
+            this.txtNombreUsu.TabIndex = 2;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(218, 307);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '*';
+            this.txtContra.Size = new System.Drawing.Size(317, 20);
+            this.txtContra.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(75, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre De Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(127, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Contraseña:";
+            // 
+            // InicioSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::JuegoQuizzReto.Properties.Resources.Galaxy_repeating;
+            this.ClientSize = new System.Drawing.Size(623, 699);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtNombreUsu);
+            this.Controls.Add(this.btnIniarSesion);
+            this.Controls.Add(this.label1);
+            this.Name = "InicioSesion";
+            this.Text = "InicioSesion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIniarSesion;
+        private System.Windows.Forms.TextBox txtNombreUsu;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ InicioSesion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoQuizzReto
+{
+    public partial class InicioSesion : Form
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniarSesion_Click(object sender, EventArgs e) {
+            MessageBox.Show("Hola");
+        }
+
+        private void btnIniarSesion_MouseLeave(object sender, EventArgs e) {
+            btnIniarSesion.ForeColor = Color.White;
+        }
+
+        private void btnIniarSesion_MouseEnter(object sender, EventArgs e) {
+            btnIniarSesion.ForeColor = Color.Black;
+        }
+    }
+}
